87b14f8e5086a33ac69999c914c3d4b4426aa12c
bathant hegazy <b.hejazy@rytalo.com>
2019-05-30 18:16:26 +0200

    second commit

--- Slider/ViewController.swift
+++ Slider/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circleImage: UIImageView!
+    @IBOutlet weak var arrow: UIImageView!
+    @IBOutlet weak var topConstraints: NSLayoutConstraint!
+    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var clickableView: UIView!
+    var constraint : NSLayoutConstraint!
+    var anotherConstraint : NSLayoutConstraint!
+//
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "bad"
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(viewClicked))
+        clickableView.addGestureRecognizer(tap)
+        constraint =  self.secondView.topAnchor.constraint(equalTo: self.firstView.topAnchor, constant: 0)
+    
+    }
+    
+    @objc func viewClicked(){
+  
+        UIView.animate(withDuration:0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0, options:[] ,
+                       animations: { () -> Void in
+                        if self.secondView.center != self.firstView.center{
+                            self.toggleViews(hide: false)
+                        }else{
+                            self.toggleViews(hide: true)
+                        }
+                       
+                        self.view.layoutIfNeeded()
+
+        }, completion: nil)
+    }
+
+    func toggleViews(hide : Bool){
+        self.constraint.isActive = hide ? false : true
+        self.arrow.transform = hide ?   CGAffineTransform.identity:  CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+        circleImage.isHidden = hide ? false : true
+        arrow.center = hide ? circleImage.center : clickableView.center
+        clickableView.backgroundColor =  hide ?  UIColor(red: 52/255, green: 116/255, blue: 189/255, alpha: 1.0) : .black
+    }
+        
     }
 
 
-}
+
 
